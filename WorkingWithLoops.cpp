7b240ebe0986e9bb37ba0788479c2a5e7dadd25c
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-12-13 20:31:40 +0300

    Построить цикл

--- WorkingWithLoops.cpp
+++ WorkingWithLoops.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+
+int main()
+{   // Print hello 9 times
+
+	int i = 1;
+
+	while (i < 10) {
+		printf("Hello\n");
+		i++;
+	}
+	
+	// Print numbers in range from 0 to 15
+
+	i = 0;
+	printf("\n");
+
+	while (i < 16) {
+		printf(" %i", i);
+		i++;
+	}
+	//Print only uneven numbers
+
+	i = 1;
+	printf("\n");
+
+	while (i < 16) {
+		printf(" %i", i);
+		i = i + 2;
+	}
+}
