5a4489e0378773adba6eeb5326ab0b807b859ec4
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-12-15 11:02:56 +0300

    выводить сумму числе в диапазоне

--- WorkingWithLoops.cpp
+++ WorkingWithLoops.cpp
@@ -5,6 +5,7 @@ int main()
 {   // Print hello 9 times
 
 	int i = 1;
+	int sum = 0;
 
 	while (i < 10) {
 		printf("Hello\n");
@@ -28,5 +29,15 @@ int main()
 	while (i < 16) {
 		printf(" %i", i);
 		i = i + 2;
+	}
+	//Print sum of numbers in range from 5 to 150
+
+	i = 5;	
+	printf("\n");
+
+	while (i < 15) {
+		sum = sum + i;
+		i++;
+		printf(" %i", sum);
 	}
 }
