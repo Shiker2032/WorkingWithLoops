931cf3648cc0c783674670cee7982ddbc1b95562
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-12-15 12:46:45 +0300

    Вторая часть построения циклов

--- WorkingWithLoops.cpp
+++ WorkingWithLoops.cpp
@@ -1,8 +1,8 @@
 
 #include <iostream>
 
-int main()
-{   // Print hello 9 times
+void WorkingWithLoops_1() {
+	// Print hello 9 times
 
 	int i = 1;
 	int sum = 0;
@@ -11,7 +11,7 @@ int main()
 		printf("Hello\n");
 		i++;
 	}
-	
+
 	// Print numbers in range from 0 to 15
 
 	i = 0;
@@ -32,7 +32,7 @@ int main()
 	}
 	//Print sum of numbers in range from 5 to 150
 
-	i = 5;	
+	i = 5;
 	printf("\n");
 
 	while (i < 15) {
@@ -40,4 +40,59 @@ int main()
 		i++;
 		printf(" %i", sum);
 	}
+
+}
+
+void WorkingWithLoops_2()
+{
+	// multiply by 5
+
+	for (int i = 1; i <= 10; i++) {
+		printf("\n%i * 5 = %i ", i, 5 * i);
+	}
+
+	int exit = 7, a = 0;
+
+	printf("\n To exit program input %i", exit);
+	// number check
+
+	while (a != exit) {
+		printf("\n Input number to check");
+		scanf_s("%i", &a);
+
+		if (a > 10) {
+			printf("\n %i is bigger than 10", a);
+		}
+		else if (a < 10) {
+			printf("\n %i is lower than 10", a);
+		}
+		else {
+			printf("\n %i equals 10", a);
+		}
+
+		if (a > 7) {
+			printf("\n %i is bigger than 7", a);
+		}
+		else if (a < 7) {
+			printf("\n %i is lower than 7", a);
+		}
+		else {
+			printf("\n %i eqauls 7");
+		}
+
+		if (a % 2 == 0) {
+			printf("\n %i can be devided by 2", a);
+		}
+		else if (a % 3 == 0) {
+			printf("\n %i can be devided by 3", a);
+		}
+		else {
+			printf("\n %i cannot be devided by 2 or 3", a);
+		}
+	}
+}
+
+int main()
+{
+	WorkingWithLoops_2();
 }
