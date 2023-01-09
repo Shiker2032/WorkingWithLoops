7c3f07f2811a982b073e1b7b5af3eb83ba025216
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-01-09 12:09:42 +0300

    Научиться использовать циклы в коде

--- WorkingWithLoops.cpp
+++ WorkingWithLoops.cpp
@@ -1,98 +1,22 @@
 
 #include <iostream>
+int main() {
+	int n = 0, sum = 0, middle = 0, min = 0, max = 0;
+	printf("Input n ");
+	scanf_s("%i", &n);
 
-void WorkingWithLoops_1() {
-	// Print hello 9 times
-
-	int i = 1;
-	int sum = 0;
-
-	while (i < 10) {
-		printf("Hello\n");
-		i++;
-	}
-
-	// Print numbers in range from 0 to 15
-
-	i = 0;
-	printf("\n");
-
-	while (i < 16) {
-		printf(" %i", i);
-		i++;
-	}
-	//Print only uneven numbers
-
-	i = 1;
-	printf("\n");
-
-	while (i < 16) {
-		printf(" %i", i);
-		i = i + 2;
-	}
-	//Print sum of numbers in range from 5 to 150
-
-	i = 5;
-	printf("\n");
-
-	while (i < 15) {
-		sum = sum + i;
-		i++;
-		printf(" %i", sum);
-	}
-
-}
-
-void WorkingWithLoops_2()
-{
-	// multiply by 5
-
-	for (int i = 1; i <= 10; i++) {
-		printf("\n%i * 5 = %i ", i, 5 * i);
-	}
-
-	int exit = 7, a = 0;
-
-	printf("\n To exit program input %i", exit);
-	// number check
-
-	while (a != exit) {
-		printf("\n Input number to check");
-		scanf_s("%i", &a);
-
-		if (a > 10) {
-			printf("\n %i is bigger than 10", a);
-		}
-		else if (a < 10) {
-			printf("\n %i is lower than 10", a);
+	for (int i = 0; i <= n; i++) {
+		sum += i;
+		if (max < i) {
+			max = i;
 		}
 		else {
-			printf("\n %i equals 10", a);
-		}
-
-		if (a > 7) {
-			printf("\n %i is bigger than 7", a);
-		}
-		else if (a < 7) {
-			printf("\n %i is lower than 7", a);
-		}
-		else {
-			printf("\n %i eqauls 7");
-		}
-
-		if (a % 2 == 0) {
-			printf("\n %i can be devided by 2", a);
-		}
-		else if (a % 3 == 0) {
-			printf("\n %i can be devided by 3", a);
-		}
-		else {
-			printf("\n %i cannot be devided by 2 or 3", a);
+			min = i;
 		}
 	}
+	printf("\nMin: %i ", min);
+	printf("Max: %i ", max);
+	printf("\nSum: %i", sum);
+	printf("\nMiddle arithmetic: %f", (float)sum / n);
 }
 
-int main()
-{
-	WorkingWithLoops_2();
-}
