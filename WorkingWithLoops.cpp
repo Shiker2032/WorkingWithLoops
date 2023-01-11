d7a7b95339b477edb7c0ff4a2cc19ed8d1e575f4
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-01-11 14:43:19 +0300

    Получать элементы с клавиатуры

--- WorkingWithLoops.cpp
+++ WorkingWithLoops.cpp
@@ -1,22 +1,28 @@
 
 #include <iostream>
 int main() {
-	int n = 0, sum = 0, middle = 0, min = 0, max = 0;
-	printf("Input n ");
+	int n = 0, sum = 0, var = 0, max = 1, min = 1;
+
+	printf("input numbers amount ");
 	scanf_s("%i", &n);
 
-	for (int i = 0; i <= n; i++) {
-		sum += i;
-		if (max < i) {
-			max = i;
+	
+	for (int i = 0; i < n; ++i)
+	{
+		
+		scanf_s("%d", &var);
+		sum += var;
+
+		if (var > max) {
+			max = var;
 		}
 		else {
-			min = i;
+			min = var;
 		}
 	}
-	printf("\nMin: %i ", min);
-	printf("Max: %i ", max);
-	printf("\nSum: %i", sum);
+
+	printf("Sum: %i", sum);
+	printf("\nMax: %i", max);
+	printf("\nMin: %i", min);
 	printf("\nMiddle arithmetic: %f", (float)sum / n);
 }
-
